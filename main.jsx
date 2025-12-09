66b8e04a36534302a864a7ea7055d5ad766434b5
pitbull968 <viggenayak4@gmail.com>
2025-12-10 02:27:48 +0900

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import Admin from './pages/Admin'
+import Admin from './Admin'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
