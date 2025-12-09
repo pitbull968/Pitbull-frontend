b14a22e143a56413644d61de32c861d74cbc9054
pitbull968 <viggenayak4@gmail.com>
2025-12-10 02:45:19 +0900

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Hero from './Hero'
 import ProductGrid from './ProductGrid'
 import AIWidget from './components/AIWidget'
-import CheckoutModal from './components/CheckoutModal'
+import CheckoutModal from './CheckoutModal'
 
 export default function App(){
   return (
