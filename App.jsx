8448e6e76226f4e6a6ce172af1ee55684bfc2e92
pitbull968 <viggenayak4@gmail.com>
2025-12-10 02:41:35 +0900

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import Hero from './Hero'
-import ProductGrid from './components/ProductGrid'
+import ProductGrid from './ProductGrid'
 import AIWidget from './components/AIWidget'
 import CheckoutModal from './components/CheckoutModal'
 
